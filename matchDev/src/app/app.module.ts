02f81d1da5875ffa39755d1f2c5930a66b34cf74
jocanijezmaj <jovanovicjovan@live.com>
2022-12-03 15:44:24 +0100

    INEPS-2: Login

--- matchDev/src/app/app.module.ts
+++ matchDev/src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- matchDev/src/app/login/login.component.ts
+++ matchDev/src/app/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor( private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form : NgForm){
+  //   const email = form.controls["username"].value ;
+  //   const password = form.controls["password"].value ;
+
+  //   this.loginService.login(email, password)
+  // }
+  // loginTest(){
+  //   this.loginService.login('test@gmail.com', '123456')
+  }
 
 }
