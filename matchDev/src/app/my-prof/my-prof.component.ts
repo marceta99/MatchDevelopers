0a5fd1a87be88daa639db563cd2feffaed4d5324
jocanijezmaj <jovanovicjovan@live.com>
2022-12-03 20:00:21 +0100

    INEPS-5:fix

--- matchDev/src/app/my-prof/my-prof.component.ts
+++ matchDev/src/app/my-prof/my-prof.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-my-prof',
   templateUrl: './my-prof.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class MyProfComponent {
 
+  
+
+  public profile = {
+    id: 1,
+    username: 'mladen.radovic',
+    name: 'mladen',
+    surname: 'radovic',
+    img: "https://www.stockvault.net/data/2009/06/09/109080/preview16.jpg",
+    info: "kur mur"
+
+  }
 }
