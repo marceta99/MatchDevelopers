0b9d8436bcc3f1bbdd511667edfb16bd9fabe3af
marceta99 <marcetaking@gmail.com>
2022-12-04 04:18:54 +0100

    Slides removed

--- matchDev/src/app/likes/likes.component.ts
+++ matchDev/src/app/likes/likes.component.ts
@@ -13,33 +13,7 @@ export class LikesComponent implements OnInit {
 
   }
 
-  likes: any[] = [{
-    id : 1,
-    userName: "marceta",
-    name : "mihailo",
-    surname: "marcetic",
-    img: "https://media.cnn.com/api/v1/images/stellar/prod/221122175737-cristiano-ronaldo.jpg?c=original",
-    info: "cele noci ja na splavu sa njom sam bio",
-    tech : [ 1, 2]
-  },
-  {
-    id : 2,
-    userName: "djoka",
-    name : "djoka",
-    surname: "roka",
-    img: "https://picsum.photos/200",
-    info: "djoka djoka djoka djoka djoka ",
-    tech : [ 1, 2]
-  },
-  {
-    id : 3,
-    userName: "joca",
-    name : "jovan",
-    surname: "jovanovic",
-    img: "https://picsum.photos/200",
-    info: "put do slave i bogatstva put do trona ",
-    tech : [ 1, 2]
-  },] ;
+  likes!: any[] ;
 
   ngOnInit(): void {
     this.slideService.getOnesThatLikedMe().subscribe((res : any)=>{
--- matchDev/src/app/matchess/matchess.component.ts
+++ matchDev/src/app/matchess/matchess.component.ts
@@ -8,34 +8,7 @@ import { SlidesService } from '../slides.service';
 })
 export class MatchessComponent implements OnInit {
 
-  matches : any[] = [{
-    id : 1,
-    userName: "marceta",
-    name : "mihailo",
-    surname: "marcetic",
-    img: "https://picsum.photos/200",
-    info: "cele noci ja na splavu sa njom sam bio",
-    tech : [ 1, 2]
-  },
-  {
-    id : 2,
-    userName: "djoka",
-    name : "djoka",
-    surname: "roka",
-    img: "https://picsum.photos/200",
-    info: "djoka djoka djoka djoka djoka ",
-    tech : [ 1, 2]
-  },
-  {
-    id : 3,
-    userName: "joca",
-    name : "jovan",
-    surname: "jovanovic",
-    img: "https://picsum.photos/200",
-    info: "put do slave i bogatstva put do trona ",
-    tech : [ 1, 2]
-  },] ;
-
+  matches! : any[] ;
 
   constructor(private slidesService : SlidesService) {
   }
